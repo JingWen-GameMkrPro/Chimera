22461a7df5874bf91aa5956a2b2069519bfb677a
JingWen <jingwenhuang1008@gmail.com>
2026-01-12 19:03:56 +0800

    feat: Loading Sci-Fi Map & Custom Normal Texture

---
+++ Assets/SciFi Warehouse Kit/Demo/Scripts/MouseComponent.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MouseComponent : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+    public Transform playerBody;
+    float xRotation = 0f;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Keyboard.current.aKey.isPressed ? -1 : (Keyboard.current.dKey.isPressed ? 1 : 0);
+        float mouseY = Keyboard.current.wKey.isPressed ? 1 : (Keyboard.current.sKey.isPressed ? -1 : 0);
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90, 90f);
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Assets/SciFi Warehouse Kit/Demo/Scripts/PlayerMovement.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController controller;
+    public float speed = 8f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    Vector3 velocity;
+    bool isGrounded;
+
+    
+     public AudioClip footStepSound;
+     public float footStepDelay;
+ 
+     private float nextFootstep = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y <0)
+        {
+            velocity.y = -2f;
+        }
+
+        float x = Keyboard.current.aKey.isPressed ? -1 : (Keyboard.current.dKey.isPressed ? 1 : 0);
+        float z = Keyboard.current.wKey.isPressed ? 1 : (Keyboard.current.sKey.isPressed ? -1 : 0);
+
+        Vector3 motion = transform.right * x + transform.forward * z;
+        controller.Move(motion * speed * Time.deltaTime);
+
+        if(Keyboard.current[Key.Space].wasPressedThisFrame && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
+         if ((x != 0 || z != 0) && isGrounded)
+         {
+             nextFootstep -= Time.deltaTime;
+             if (nextFootstep <= 0) 
+                {
+                 GetComponent<AudioSource>().PlayOneShot(footStepSound, 0.7f);
+                 nextFootstep += footStepDelay;
+                }
+             }
+         }
+}
+
+
