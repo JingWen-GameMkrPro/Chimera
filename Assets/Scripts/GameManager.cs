e7fc6e25138adfbb2068cf3b899b58a0592fc6b1
JingWen <jingwenhuang1008@gmail.com>
2026-06-20 19:51:30 +0800

    e04

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,10 +1,22 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GameManager: MonoBehaviour
 {
+    [SerializeField]
+    CanvasGroup uiCanvas;
+
     private void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        if (Keyboard.current != null && Keyboard.current.digit1Key.wasPressedThisFrame)
+        {
+            uiCanvas.alpha = uiCanvas.alpha == 1 ? 0 : 1;
+        }
     }
 }
